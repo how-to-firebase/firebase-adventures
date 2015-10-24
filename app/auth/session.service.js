623b2f511cb7ecc383839102a9ca61e44bf4b315
Viktor Zozulyak <zozulyakviktor@gmail.com>
2015-10-24 11:33:32 +0300

    login workarounds

--- app/auth/session.service.js
+++ app/auth/session.service.js
@@ -1,13 +1,23 @@
 angular.module('fireAdventures')
   .factory('Session', function(Auth, $q) {
-    var sessionDefer = $q.defer();
+    var session,
+        sessionDefer;
 
     Auth.$onAuth(function(authData) {
-      sessionDefer.resolve(authData);
+      session = authData;
+      // if someone is already waiting for session - we return it
+      if (sessionDefer) {
+        sessionDefer.resolve(authData);
+      }
     });
 
     return {
       getSession: function() {
+        sessionDefer = $q.defer();
+        // if session is already set - we return it
+        if (session) {
+          sessionDefer.resolve(session);
+        }
         return sessionDefer.promise;
       }
     };
